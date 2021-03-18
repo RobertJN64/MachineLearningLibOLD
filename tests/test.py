6f53723b2f7038f5d6836a903f8ce59377087092
RobertJN <roberjnies@gmail.com>
2021-03-17 22:32:45 -0400

    fixed test.py file directory

--- tests/test.py
+++ tests/test.py
@@ -2,40 +2,40 @@ from MachineLearning import GeneticNets as gn
 from MachineLearning import GeneticEvolution as ge
 from MachineLearning import NetRender as nr
 
-#config
-fname = "tests/files/titanic.json"
-generations = 10
-populationSize = 10
-trainSize = 400
-testSize = 100
-evoRate = 4
+def test(fname = "venv/tests/files/titanic.json"):
+    #config
+    generations = 10
+    populationSize = 10
+    trainSize = 400
+    testSize = 100
+    evoRate = 4
 
-#specific config - midnodes
-midDepth = 10
-midWidth = 2
+    #specific config - midnodes
+    midDepth = 10
+    midWidth = 2
 
-screen = nr.screen()
-rSettings = nr.stdSettings(screen)
-rSettings["settings"]["vdis"] = 15
+    screen = nr.screen()
+    rSettings = nr.stdSettings(screen)
+    rSettings["settings"]["vdis"] = 15
 
-dataset, trainset, testset, metadata = ge.loadDataset(fname, testSize)
+    dataset, trainset, testset, metadata = ge.loadDataset(fname, testSize)
 
-NetDB = gn.Random(metadata.inputs, metadata.outputs, 20, 2, 5) #creates the neural nets
+    NetDB = gn.Random(metadata.inputs, metadata.outputs, 20, 2, 5) #creates the neural nets
 
-bestNet = [None, 0]
+    bestNet = [None, 0]
 
-import time
-startTime = time.time()
+    import time
+    startTime = time.time()
 
-for genCount in range(0, generations):
-    evoRate = evoRate * 0.95
-    NetDB, best, bestscore, truescore = ge.Test(NetDB, dataset, trainset, trainSize, testset, renderSettings = rSettings)
-    if truescore > bestNet[1]:
-        bestNet[0] = best
-    NetDB = ge.evolve(NetDB, evoRate)
-    screen.bestNet(best, bestscore, truescore)
+    for genCount in range(0, generations):
+        evoRate = evoRate * 0.95
+        NetDB, best, bestscore, truescore = ge.Test(NetDB, dataset, trainset, trainSize, testset, renderSettings = rSettings)
+        if truescore > bestNet[1]:
+            bestNet[0] = best
+        NetDB = ge.evolve(NetDB, evoRate)
+        screen.bestNet(best, bestscore, truescore)
 
-gn.saveNets([bestNet[0]], "scripts/files/testnet", "Nets from file: " + fname, 2)
-nr.stop()
+    gn.saveNets([bestNet[0]], "testnet", "Nets from file: " + fname, 2)
+    nr.stop()
 
-print("Execution time: ", time.time() - startTime)
+    print("Execution time: ", time.time() - startTime)
