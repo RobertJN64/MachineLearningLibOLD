f8de89e68d59a0d22b05b256a0afb984bed7882d
RobertJN <roberjnies@gmail.com>
2021-03-17 22:50:47 -0400

    Testing how github updates work

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ from MachineLearning import GeneticEvolution as ge
 from MachineLearning import NetRender as nr
 
 def test(fname = "venv/tests/files/titanic.json"):
-    print("Running test script")
+    print("Running test script v2")
     #config
     generations = 10
     populationSize = 10
